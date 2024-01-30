aab7959a0ecb11f5309b9b07ae37df46d7838382
Roberts Janis Siklis <roberts.siklis@gmail.com>
2024-01-30 10:05:35 +0200

    Solution for 2017 Day 1 Part 1

--- 2017/AOCDay1/AOCDay1Part1/main.cpp
+++ 2017/AOCDay1/AOCDay1Part1/main.cpp
@@ -1,7 +1,40 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <deque>
+int main()
+{
+    std::ifstream ifs;
 
-int main() {
-	std::cout << "Hello World!" << '\n';
-	std::cout << "Will this update in git?" << '\n';
-	return 0;
+    ifs.open("input.txt");
+
+    if (!ifs.is_open()) {
+        std::cout << "Failed opening input file!";
+        return 1;
+    }
+
+    std::deque<int> input;
+    std::deque<int>* inputPtr = &input;
+    char c;
+
+    while (ifs.get(c)) {
+        input.push_back(c - '0');
+    }
+    ifs.close();
+    int total = 0;
+
+
+    for (int i = 0; i < input.size() - 1; ++i) {
+        if (inputPtr + i + 1 == nullptr) {
+            break;
+        }
+        if (input.at(i) == input.at(i + 1)) {
+            total += input.at(i);
+        }
+    }
+
+    if (input.front() == input.back()) {
+        total += input.front();
+    }
+
+    std::cout << total;
 }
