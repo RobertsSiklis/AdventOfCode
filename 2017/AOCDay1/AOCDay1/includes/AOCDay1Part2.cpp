4c390d72d1b0edcf5eee2906bc11c7c90c2f73f9
Roberts Janis Siklis <roberts.siklis@gmail.com>
2024-01-31 13:17:02 +0200

    Solved 2017Day1Part2

--- 2017/AOCDay1/AOCDay1/includes/AOCDay1Part2.cpp
+++ 2017/AOCDay1/AOCDay1/includes/AOCDay1Part2.cpp
@@ -4,16 +4,16 @@ int AdventOfCodeDay1Part2(const std::deque<int>* const inputPtr) {
 
     int stepsForward = inputPtr->size() / 2;
     int total = 0;
-    for (int i = 0; i < inputPtr->size() - 1; ++i) {
-        if (inputPtr + i + stepsForward == nullptr) {
-            //Used for checking how many steps left when entering new Circle Round
-            int stepsIntoNextCircleRound = inputPtr->size() - i - stepsForward;
-            std::cout << stepsIntoNextCircleRound;
-            //if (inputPtr->at(i) == inputPtr->at(stepsIntoNextCircleRound)) {
-            //    total += inputPtr->at(i);
-            //}     
-        //} else if (inputPtr->at(i) == inputPtr->at(i + stepsForward)) {
-         //   total += inputPtr->at(i);
+    for (int i = 0; i < inputPtr->size(); ++i) {
+        if (inputPtr + i + stepsForward > inputPtr + inputPtr->size() - 1) {
+            //Checking how many steps left when entering new Circle Round
+            int stepsLeftToGetToNextCircleRound = inputPtr->size() - i;
+            int stepsIntoNextCircleRound = stepsForward - stepsLeftToGetToNextCircleRound;
+            if (inputPtr->at(i) == inputPtr->at(stepsIntoNextCircleRound)) {
+                total += inputPtr->at(i);
+            }     
+        } else if (inputPtr->at(i) == inputPtr->at(i + stepsForward)) {
+            total += inputPtr->at(i);
         }
     }
 
